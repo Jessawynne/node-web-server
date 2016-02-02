e546f4759185b7f0e4b64b96ef9a533f6de4cf6e
Jessawynne <jessawynne@gmail.com>
2016-02-02 11:27:46 -0600

    refactor with express.js

--- server.js
+++ server.js
@@ -1,61 +1,40 @@
-//const http = require('http');
-
-//'use strict';
-
-//http.createServer((req, res) => {
-
-  //console.log(req.method, req.url);
-
-  ////response.writeHead(); in docs
-  //res.writeHead(200, {
-    //'Content-type': 'text/html'
-  //});
-
-  ////res.end('Done!');
-  //res.end('<<h1>HEY</h1>');
-
-//}).listen(3000, () => {
-
-  //console.log('Node.js server started. Listening on port 3000');
-
-//});
-////3000 is the port that we will listen to
-
 'use strict';
 
-const http = require('http');
+const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-	console.log(req.method, req.url);
+app.get('/hello', (req, res) => {
+  const msg = `<h1>HELLO WORLD!</h1>
+               <h2>GOODBYE WORLD!</h2>`;
 
-  if (req.url === '/hello') {
-    const msg = `<h1>HELLO WORLD!</h1>
-                 <h2>GOODBYE WORLD!</h2>`;
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
 
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-
-    //chunk response by character
-    msg.split('').forEach((char, i) => {
-      setTimeout(() => {
-        res.write(char);
-      }, 1000 * i);
-    });
-
-    //wait for all characters to be sent
+  //chunk response by character
+  msg.split('').forEach((char, i) => {
     setTimeout(() => {
-      res.end();
-    }, msg.length * 1000 + 2000);
-  } else if (req.url === '/random') {
-    res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    //http status code
-    res.end('Access Denied!');
-  }
+      res.write(char);
+    }, 1000 * i);
+  });
 
-}).listen(PORT, () => {
+  //wait for all characters to be sent
+  setTimeout(() => {
+    res.end();
+  }, msg.length * 1000 + 2000);
+});
+
+app.get('/random', (req, res) => {
+  res.send(Math.random().toString());
+});
+
+//all = any verbs, * = everything
+app.all('*', (req, res) => {
+  res
+    .status(403)
+    .send('Access Denied!');
+});
+
+app.listen(PORT, () => {
   console.log('Node.js server started. Listening on port ${PORT}');
 });
